aaaff00c9a5d84c89c674ec9a457c4df2d2f0e07
Ahmad Shahwan <ahmad.shahwan@gmail.com>
2024-12-04 10:57:04 +0100

    Save to local storage

--- script.js
+++ script.js
@@ -3,8 +3,18 @@ const newTodoItemBlock = document.getElementById('new-item');
 const editTodoItemTitleEl = document.getElementById('edit-todo-item-title');
 const newTodoItemTitleEl = document.getElementById('new-todo-item-title')
 const todoListEl = document.getElementById('todo-list');
+const items = load();
 
 let itemEdited;
+
+function load() {
+    const storage = localStorage.getItem('todo_list_items');
+    return storage ? JSON.parse(storage) : [];
+}
+
+function save() {
+    localStorage.setItem('todo_list_items', JSON.stringify(items));
+}
 
 function editItem(el) {
     editTodoItemTitleEl.value = el.innerText;
@@ -25,14 +35,33 @@ function cancelEdit() {
     resetEdit();
 }
 
+function indexOfListItem(element) {
+    return [...todoListEl.querySelectorAll('li')].indexOf(element);
+}
+
 function confirmEdit() {
-    itemEdited.innerText = editTodoItemTitleEl.value;
+    const index = indexOfListItem(itemEdited.parentElement);
+    items[index] = itemEdited.innerText = editTodoItemTitleEl.value;
+    save();
     resetEdit();
+}
+
+function removeElement(element) {
+    const index = indexOfListItem(element);
+    items.splice(index, 1);
+    save();
+    element.remove();
 }
 
 function addItem() {
     const title = newTodoItemTitleEl.value;
+    items.push(title);
+    save();
+    appendTitle(title)
     newTodoItemTitleEl.value = '';
+}
+
+function appendTitle(title) {
     const listItemEl = document.createElement('li');
     const listItemTitleEl = document.createElement('span');
     const listItemDeleteEl = document.createElement('button');
@@ -40,7 +69,7 @@ function addItem() {
     listItemTitleEl.innerText = title;
     listItemDeleteEl.innerText = 'Delete';
     listItemEditEl.innerText = 'Edit';
-    listItemDeleteEl.addEventListener('click', () => listItemEl.remove());
+    listItemDeleteEl.addEventListener('click', () => removeElement(listItemEl));
     listItemEditEl.addEventListener('click', () => editItem(listItemTitleEl));
     listItemEl.append(listItemTitleEl);
     listItemEl.append(listItemDeleteEl);
@@ -56,6 +85,8 @@ function addItemOnEnter(event) {
 function confirmEditOnEnter(event) {
     event.key === 'Enter' && confirmEdit();
 }
+
+items.forEach(appendTitle);
 
 document.getElementById('new-todo-item-title').addEventListener('keypress', addItemOnEnter);
 document.getElementById('new-todo-item-add').addEventListener('click', addItem);
